b29b9cedefd1668d517b0e991045b0037cae150f
LecygneNoir <git@lecygnenoir.info>
2019-08-11 10:57:12 +0200

    Patch the check for playlist exist on Youtube, fix #39

--- lib/yt_upload.py
+++ lib/yt_upload.py
@@ -168,7 +168,7 @@ def get_playlist_by_name(youtube, playlist_name):
         maxResults=50
     ).execute()
     for playlist in response["items"]:
-        if playlist["snippet"]['title'] == playlist_name:
+        if playlist["snippet"]['title'].encode('utf8') == str(playlist_name):
             return playlist['id']
 
 
